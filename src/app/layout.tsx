eab90521c21ebc3b5c3fb4f60f7af4d6105447b4
Erick Friis <erickfriis@gmail.com>
2023-03-14 08:59:25 -0700

    configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'e3k',
-  description: 'Erick\'s chatbot',
-}
+  title: "e3k",
+  description: "Erick's chatbot",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="h-full bg-white">
+      <body className="h-full">{children}</body>
     </html>
-  )
+  );
 }
